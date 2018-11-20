b12b1aff6ac261c7c107b398c7320927016bc73c
Benjamin Gröhbiel <ben@boclips.com>
2018-11-20 17:48:30 +0000

    Minor refactor
    Signed-off-by: José Carlos Valero Sánchez <josecarlos@boclips.com>

--- src/main/kotlin/com/boclips/api/gateway/config/RoutesConfig.kt
+++ src/main/kotlin/com/boclips/api/gateway/config/RoutesConfig.kt
@@ -1,9 +1,12 @@
 package com.boclips.api.gateway.config
 
+import com.boclips.api.gateway.application.RequestExtractor
+import org.springframework.cloud.gateway.filter.GlobalFilter
 import org.springframework.cloud.gateway.route.RouteLocator
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
 
 @Configuration
 class RoutesConfig {
@@ -21,5 +24,24 @@ class RoutesConfig {
                 }
 
                 .build()
+    }
+
+    @Order(1)
+    @Bean
+    fun forwardedHeadersFilter(): GlobalFilter {
+
+        return GlobalFilter { exchange, chain ->
+            val requestDomain = RequestExtractor().extract(exchange.request)
+            val request = exchange.request.mutate()
+                    .headers { httpHeaders ->
+                        httpHeaders.remove("Forwarded")
+                        httpHeaders.add("x-forwarded-host", requestDomain.host)
+                        httpHeaders.add("x-forwarded-port", requestDomain.port.toString())
+                        httpHeaders.add("x-forwarded-proto", requestDomain.protocol)
+                    }
+                    .build()
+
+            chain.filter(exchange.mutate().request(request).build())
+        }
     }
 }
