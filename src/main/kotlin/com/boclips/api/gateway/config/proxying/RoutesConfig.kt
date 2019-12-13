62d2e12e83a431023955dacada5381d6ce3cb787
Rebecca Appleyard <rebecca@boclips.com>
2019-12-13 09:56:46 +0000

    Update independent-accounts endpoint route
    [#170014046]
    
    Signed-off-by: Antony O'Neill <antony@boclips.com>

--- src/main/kotlin/com/boclips/api/gateway/config/proxying/RoutesConfig.kt
+++ src/main/kotlin/com/boclips/api/gateway/config/proxying/RoutesConfig.kt
@@ -59,7 +59,7 @@ class RoutesConfig {
                 uri(props.userServiceUrl)
             }
             route {
-                path("/v1/independent-organisations/**")
+                path("/v1/independent-accounts/**")
                 uri(props.userServiceUrl)
             }
             route {
