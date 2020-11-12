fb807f53bdf1f5b81f5e2034f105f56583aac25a
Marcin Janik <marcin@boclips.com>
2020-11-12 15:52:08 +0100

    Add missing expires_in property in TokenResponse
    [#175413436]
    
    Signed-off-by: Rebecca Appleyard <rebecca@boclips.com>

--- src/main/kotlin/com/boclips/api/gateway/config/TokenResponse.kt
+++ src/main/kotlin/com/boclips/api/gateway/config/TokenResponse.kt
@@ -2,6 +2,7 @@ package com.boclips.api.gateway.config
 
 data class TokenResponse(
     val access_token: String?,
+    val expires_in: String?,
     val refresh_expires_in: String?,
     val refresh_token: String?,
     val token_type: String?,
