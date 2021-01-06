145d5cac8be23e9c66bffa6b48b87ec6947038b1
Matt Jones <matt@boclips.com>
2021-01-06 17:48:32 +0000

    Further attempt to fix trace ids being null
    - Annoyingly the previous method worked when running locally, but in
    staging would give a null trace id
    
    [#176344589]

--- src/main/kotlin/com/boclips/api/gateway/infrastructure/CustomGlobalLogger.kt
+++ src/main/kotlin/com/boclips/api/gateway/infrastructure/CustomGlobalLogger.kt
@@ -2,7 +2,6 @@ package com.boclips.api.gateway.infrastructure
 
 import com.boclips.api.gateway.domain.service.TraceProvider
 import mu.KLogging
-import org.springframework.core.Ordered
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
 import org.springframework.web.server.WebFilter
@@ -14,7 +13,7 @@ class CustomGlobalLogger(private val traceProvider: TraceProvider) : WebFilter {
     companion object : KLogging()
 
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
-        return chain.filter(exchange).doFinally { _ ->
+        return chain.filter(exchange).doOnRequest { _ ->
             logger.info {
                 "${exchange.request.methodValue} request for ${exchange.request.uri} with trace id: ${traceProvider.getTraceId()?.value} gave status: ${
                     exchange.response.statusCode
