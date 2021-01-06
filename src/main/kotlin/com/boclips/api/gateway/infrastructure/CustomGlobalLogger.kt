eef7bfd9e241c52e64229d5b1e62f237fdce33dc
Matt Jones <matt@boclips.com>
2021-01-06 16:59:05 +0000

    Fix trace id being null when logging
    [#176344589]

--- src/main/kotlin/com/boclips/api/gateway/infrastructure/CustomGlobalLogger.kt
+++ src/main/kotlin/com/boclips/api/gateway/infrastructure/CustomGlobalLogger.kt
@@ -2,18 +2,18 @@ package com.boclips.api.gateway.infrastructure
 
 import com.boclips.api.gateway.domain.service.TraceProvider
 import mu.KLogging
-import org.springframework.cloud.gateway.filter.GatewayFilterChain
-import org.springframework.cloud.gateway.filter.GlobalFilter
 import org.springframework.core.Ordered
 import org.springframework.stereotype.Component
 import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
 
 @Component
-class CustomGlobalLogger(private val traceProvider: TraceProvider) : GlobalFilter, Ordered {
+class CustomGlobalLogger(private val traceProvider: TraceProvider) : WebFilter {
     companion object : KLogging()
 
-    override fun filter(exchange: ServerWebExchange, chain: GatewayFilterChain): Mono<Void> {
+    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
         return chain.filter(exchange).doFinally { _ ->
             logger.info {
                 "${exchange.request.methodValue} request for ${exchange.request.uri} with trace id: ${traceProvider.getTraceId()?.value} gave status: ${
@@ -21,10 +21,6 @@ class CustomGlobalLogger(private val traceProvider: TraceProvider) : GlobalFilte
                 }"
             }
         }
-    }
-
-    override fun getOrder(): Int {
-        return Int.MIN_VALUE
     }
 }
 
