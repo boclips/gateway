6361a2e0f813032aaafb5092cb795360c365e494
Matt Jones <matt@boclips.com>
2021-01-06 18:15:12 +0000

    Only log requests with valid traces
    Essentially filtering out any health monitoring requests
    
    [#176344589]

--- src/main/kotlin/com/boclips/api/gateway/infrastructure/WebRequestLogger.kt
+++ src/main/kotlin/com/boclips/api/gateway/infrastructure/WebRequestLogger.kt
@@ -9,15 +9,17 @@ import org.springframework.web.server.WebFilterChain
 import reactor.core.publisher.Mono
 
 @Component
-class CustomGlobalLogger(private val traceProvider: TraceProvider) : WebFilter {
+class WebRequestLogger(private val traceProvider: TraceProvider) : WebFilter {
     companion object : KLogging()
 
     override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
         return chain.filter(exchange).doOnRequest { _ ->
-            logger.info {
-                "${exchange.request.methodValue} request for ${exchange.request.uri} with trace id: ${traceProvider.getTraceId()?.value} gave status: ${
-                    exchange.response.statusCode
-                }"
+            traceProvider.getTraceId()?.let { traceId ->
+                logger.info {
+                    "${exchange.request.methodValue} request for ${exchange.request.uri} with trace id: ${traceId.value} gave status: ${
+                        exchange.response.statusCode
+                    }"
+                }
             }
         }
     }
